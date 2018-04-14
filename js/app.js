165e244391e36b738b64f170d80e75e2af63fa96
ctben <ctben.lf@gmail.com>
2018-04-15 04:52:50 +0700

    Added css animated to header/footer

--- js/app.js
+++ js/app.js
@@ -19,24 +19,27 @@ jQuery(document).ready(function($) {
 
   new WOW().init();
 
+$(document).on("scroll", onScroll);
+
   // Smoth scroll on page hash links
-  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
+  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function(e) {
+    e.preventDefault();
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       if (target.length) {
-        var top_space = 61;
+        var top_space = 50;
 
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
-
-          if( ! $('#header').hasClass('header-fixed') ) {
-            top_space = top_space - 20;
-          }
-        }
+        // if ($('#header').length) {
+        //   top_space = $('#header').outerHeight();
+            
+        //   if( ! $('#header').hasClass('header-fixed') ) {
+        //     top_space = top_space - 20;
+        //   }
+        // }
 
         $('html, body').animate({
           scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
+        }, 1000, 'easeInOutExpo');
 
         if ($(this).parents('.nav-menu').length) {
           $('.nav-menu .menu-active').removeClass('menu-active');
@@ -53,6 +56,47 @@ jQuery(document).ready(function($) {
     }
   });
 });
+
+// $(document).ready(function () {
+//     $(document).on("scroll", onScroll);
+    
+//     //smoothscroll
+//     $('a[href^="#"]').on('click', function (e) {
+//         e.preventDefault();
+//         $(document).off("scroll");
+        
+//         $('a').each(function () {
+//             $(this).removeClass('active');
+//         })
+//         $(this).addClass('active');
+      
+//         var target = this.hash,
+//             menu = target;
+//         $target = $(target);
+//         var top_space = 61;
+//         $('html, body').stop().animate({
+//             'scrollTop': $target.offset().top - top_space
+//         }, 500, 'swing', function () {
+//             window.location.hash = target - top_space;
+//             $(document).on("scroll", onScroll);
+//         });
+//     });
+// });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#navbarResponsive a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top - 150 <= scrollPos && refElement.position().top - 150 + refElement.height() > scrollPos) {
+            $('#navbarResponsive ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
 
 particlesJS('particles-js',
   
@@ -94,7 +138,7 @@ particlesJS('particles-js',
         }
       },
       "size": {
-        "value": 5,
+        "value": 5.68,
         "random": true,
         "anim": {
           "enable": false,
@@ -105,10 +149,10 @@ particlesJS('particles-js',
       },
       "line_linked": {
         "enable": true,
-        "distance": 150,
+        "distance": 168,
         "color": "#ffffff",
         "opacity": 0.28,
-        "width": 1
+        "width": 2
       },
       "move": {
         "enable": true,
